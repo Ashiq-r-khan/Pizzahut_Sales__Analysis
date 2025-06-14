566c547e44192e38133f5e3ad0f358db55e56ff4
Md. Ashiqur Rahman Khan <ashiqurrahmankhan04@gmail.com>
2025-06-15 00:21:36 +0600

    Add files via upload

---
+++ Scripts/Qus_9.sql
@@ -0,0 +1,16 @@
+-- Group the orders by date and 
+-- calculate the average number of pizzas ordered per day
+
+-- CTE to aggregate the total quantity of pizzas ordered for each order date
+
+WITH ordered_quantity AS (
+    SELECT 
+        o.order_date, 
+        SUM(od.quantity) AS quantity
+    FROM orders AS o
+    JOIN order_details AS od ON o.order_id = od.order_id
+    GROUP BY o.order_date
+)
+
+SELECT ROUND(AVG(quantity), 0) AS avg_order
+FROM ordered_quantity;
